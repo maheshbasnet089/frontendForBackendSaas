49bef65bf1aa07d17aa6e387a32ff08fb9c8a25e
maheshbasnet089 <basnetmanish089@gmail.com>
2025-05-15 21:01:04 +0545

    first commit

---
+++ src/pages/SinglePage.jsx
@@ -0,0 +1,14 @@
+
+import Navbar from "../components/Navbar"
+
+
+function SinglePage(){
+    return(
+        <>
+       <Navbar />
+        <h1>this is single page</h1>
+        </>
+    )
+}
+
+export default SinglePage
