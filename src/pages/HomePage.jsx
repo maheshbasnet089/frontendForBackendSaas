b15d9b4e5c300105641598f499f1979b322c1508
maheshbasnet089 <basnetmanish089@gmail.com>
2025-05-16 20:53:03 +0545

    learn cors

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
-import Button from "../components/Button"
+import { useEffect } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
+import axios from 'axios'
 
 function HomePage(){
+  const fetchBooks  = async ()=>{
+   const response = await axios.get("http://localhost:4000/api/books/")
+  }
+   useEffect(()=>{
+    fetchBooks()
+   },[])
     return(
        <>
       <Navbar />
