d54bd561167f61487541dc7a88ae1d881c191a7c
maheshbasnet089 <basnetmanish089@gmail.com>
2025-05-20 20:25:48 +0545

    add home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,20 @@
 import { Link } from "react-router-dom"
 
 
-function Card(){
+function Card({book}){
+    console.log(book)
     return(
-<Link to="/single-page">
+<Link to={`/single-page/${book.id}`}>
     <div className="flex px-3 py-3">
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
         <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <div className="font-bold text-xl mb-2">{book.bookName}</div>
         <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
-            perferendis eaque, exercitationem praesentium nihil.
+            {book.bookAuthor}
         </p>
         </div>
         <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#{book.price}</span>
         </div>
     </div>
     </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,30 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 import axios from 'axios'
 
 function HomePage(){
+  const [books,setBooks] = useState([])
   const fetchBooks  = async ()=>{
    const response = await axios.get("http://localhost:4000/api/books/")
+   setBooks(response.data.datas)
   }
    useEffect(()=>{
     fetchBooks()
    },[])
+
+
     return(
        <>
       <Navbar />
       <div className="flex flex-wrap">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {
+          books.map(function(book){ // books vanne state lai loop garyo using map higher order function, arrayToLoop.map(function(arrayKoEachItem){ return ()})
+            return (
+              <Card book={book} />
+            )
+          })
+        }
       </div>
        </>
     )
