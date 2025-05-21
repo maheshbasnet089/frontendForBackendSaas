9007f60fe6fad1beb5192d8cbcdb3998a5f7e841
maheshbasnet089 <basnetmanish089@gmail.com>
2025-05-21 20:32:30 +0545

    single,delete vayo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 
 
 function Card({book}){
-    console.log(book)
     return(
 <Link to={`/single-page/${book.id}`}>
     <div className="flex px-3 py-3">
--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar"
 
 
 function CreatePage(){
+    
     return (
        <Navbar />
     )
